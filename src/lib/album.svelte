c9dc14b48e275759ed9643238a8757bd26cc44c7
justpeterpan <peter.busch@posteo.net>
2023-11-04 12:00:47 +0100

    feat: add album component

---
+++ src/lib/album.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import MusicServices from '$lib/music-services.svelte';
+	import Rating from './rating.svelte';
+
+	type Album = {
+		name: string;
+		artist: string;
+		releaseDate: string;
+		listenedOn: string;
+		rating: number;
+		cover: string;
+		spotify: string;
+		appleMusic: string;
+	};
+
+	export let album: Album;
+</script>
+
+<div class="flex flex-col sm:flex-row sm:space-x-8 space-y-4 sm:space-y-0">
+	<div
+		class="relative w-full sm:w-28 aspect-square before:border-2 before:border-white before:top-1 before:left-1.5 before:w-full before:h-full before:absolute before:rounded-lg before:bg-[#1a1a1a] hover:before:bg-white before:transition-colors before:duration-500 before:ease-in-out"
+	>
+		<img
+			class="absolute inset-0 rounded-lg border-2 border-white object-cover w-full h-full"
+			src={album.cover}
+			alt={album.name}
+		/>
+	</div>
+	<div class="flex flex-col justify-center">
+		<div class="flex flex-row flex-wrap gap-2">
+			<h3 class="font-bold mb-0">{album.artist} - {album.name}</h3>
+			<MusicServices am={album.appleMusic} sp={album.spotify} />
+		</div>
+		<div class="font-extralight">
+			<strong>Listened on:</strong>
+			{album.listenedOn}
+		</div>
+		<div class="mb-2 font-extralight">
+			<strong>Release date:</strong>
+			{album.releaseDate}
+		</div>
+		<Rating rating={album.rating} />
+	</div>
+</div>
--- src/lib/music-services.svelte
+++ src/lib/music-services.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex flex-row gap-1 place-self-center">
 	<a href="https://music.apple.com/de/album/{am}" target="_blank"
-		><img src={AM} alt="apple music logo" class="w-4 ml-4" /></a
+		><img src={AM} alt="apple music logo" class="w-4" /></a
 	>
 	<a href="https://open.spotify.com/album/{sp}" target="_blank"
 		><img src={SP} alt="spotify logo" class="w-4" /></a
