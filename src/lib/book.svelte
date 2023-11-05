c77b26b373721468c006f09603291956d1590481
justpeterpan <peter.busch@posteo.net>
2023-11-05 20:02:15 +0100

    refactor(Book): redesign book layout

---
+++ src/lib/book.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	export let cover: string;
+</script>
+
+<div class="btn w-40">
+	<img src={cover} alt="book cover" />
+</div>
+
+<style>
+	:root {
+		--slate-color: white;
+		--blue-color: white;
+		--pink-color: white;
+	}
+
+	.btn {
+		color: white;
+		position: relative;
+		align-self: center;
+		border: 2px #ffffff solid;
+		border-image: linear-gradient(45deg, #ffffff, #ffffff);
+		border-image-slice: 1 0 0 1;
+		z-index: 1;
+		box-shadow: 10px 10px rgba(255, 255, 255, 0);
+	}
+	.btn:before,
+	.btn:after {
+		border: 2px #ffffff solid;
+		content: '';
+		display: block;
+		position: absolute;
+		z-index: -1;
+	}
+	.btn:before {
+		border-image: linear-gradient(45deg, #ffffff 0%, #ffffff 100%);
+		border-image-slice: 1 1 0 1;
+		right: -10px;
+		top: 3px;
+		width: 10px;
+		height: 101.7%;
+		transform: skewY(45deg);
+	}
+	.btn:after {
+		border-image: linear-gradient(45deg, #ffffff, #ffffff);
+		border-image-slice: 1 0 1 1;
+		bottom: -10px;
+		right: -7px;
+		width: 102.5%;
+		height: 10px;
+		transform: skewX(45deg);
+	}
+</style>
--- src/routes/reading/+page.svelte
+++ src/routes/reading/+page.svelte
@@ -3,61 +3,26 @@
 	import TheManWhoDiedTwice from '$lib/assets/books/ro_tmwdt.webp';
 	import TheBulletThatMissed from '$lib/assets/books/ro_tbtm.webp';
 	import TheLastDevilToDie from '$lib/assets/books/ro_tldtd.webp';
+	import Book from '$lib/book.svelte';
 </script>
 
 <div>
 	<h2 class="text-4xl font-black -mb-2 drop-shadow-md">Reading</h2>
 	<section class="mb-20">
-		<div class="bg-white w-full p-10 rounded-md">
+		<div class="w-full pt-10 rounded-md">
 			<div class="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 gap-10">
-				<div class="shad w-40 rounded-sm with-texture relative">
-					<img src={TheLastDevilToDie} alt="book cover" class="w-40 rounded-sm object-cover" />
-				</div>
+				<Book cover={TheLastDevilToDie} />
 			</div>
 		</div>
 	</section>
 	<h2 class="text-4xl font-black -mb-2 drop-shadow-md">Read</h2>
 	<section>
-		<div class="bg-white w-full p-10 rounded-md">
+		<div class=" w-full pt-10 rounded-md">
 			<div class="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 gap-10">
-				<div class="shad w-40 rounded-sm with-texture relative">
-					<img src={ThursdayMurderClub} alt="book cover" class="w-40 rounded-sm object-cover" />
-				</div>
-				<div class="shad w-40 rounded-sm with-texture relative">
-					<img src={TheManWhoDiedTwice} alt="book cover" class="w-40 rounded-sm object-cover" />
-				</div>
-				<div class="shad w-40 rounded-sm with-texture relative">
-					<img src={TheBulletThatMissed} alt="book cover" class="w-40 rounded-sm object-cover" />
-				</div>
+				<Book cover={ThursdayMurderClub} />
+				<Book cover={TheManWhoDiedTwice} />
+				<Book cover={TheBulletThatMissed} />
 			</div>
 		</div>
 	</section>
 </div>
-
-<!-- 160 * 238 -->
-
-<style>
-	.with-texture::after {
-		content: '';
-		position: absolute;
-		inset: 0px;
-		border-radius: 2px;
-		pointer-events: none;
-		box-shadow: rgba(15, 15, 15, 0.1) 1px 1px 1px 0px inset;
-		background: linear-gradient(
-			90deg,
-			rgba(0, 0, 0, 0.118) 0.65%,
-			rgba(255, 255, 255, 0.2) 1.53%,
-			rgba(255, 255, 255, 0.1) 2.38%,
-			rgba(0, 0, 0, 0.05) 3.26%,
-			rgba(255, 255, 255, 0.14) 5.68%,
-			rgba(244, 244, 244, 0) 6.96%
-		);
-	}
-
-	.shad {
-		box-shadow: rgba(0, 0, 0, 0.15) 0px 1.1px 1.5px, rgba(0, 0, 0, 0.1) 0px 2.8px 3.9px,
-			rgba(0, 0, 0, 0.08) 0px 5.8px 7.9px, rgba(0, 0, 0, 0.06) 0px 12.0455px 16.4px,
-			rgba(0, 0, 0, 0.04) 0px 33px 45px;
-	}
-</style>
